0a351ce06571011bde5bb796fd5dacab1a058752
Biju Philip <bijuphilip0@gmail.com>
2018-10-28 23:57:53 -0500

    uses Lombok to avoid too much typing/errors

--- springs/src/main/java/com/springs/springs/hibernate/Employee.java
+++ springs/src/main/java/com/springs/springs/hibernate/Employee.java
@@ -1,36 +1,22 @@
 package com.springs.springs.hibernate;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
+@Data
 public class Employee {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+
     private String name;
+
     private String phone;
 }
-
 
