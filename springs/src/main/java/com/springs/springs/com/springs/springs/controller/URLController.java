00c0d69ed89de560ab11f2403b98a076eb8d626c
Biju Philip <bijuphilip0@gmail.com>
2018-10-28 23:57:10 -0500

    adds @Controller annotation to fix binding result issue

--- springs/src/main/java/com/springs/springs/com/springs/springs/controller/URLController.java
+++ springs/src/main/java/com/springs/springs/com/springs/springs/controller/URLController.java
@@ -1,22 +1,26 @@
 package com.springs.springs.com.springs.springs.controller;
 
 import com.springs.springs.hibernate.Employee;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class URLController {
+
     @GetMapping({"/", "/index"})
-    public String index1(Model model){
-        model.addAttribute("employee",new Employee());
+    public String index(Model model) {
+
+        model.addAttribute("employee", new Employee());
         return "index";
     }
 
     @PostMapping("/result")
-    public String result(@ModelAttribute Employee employee){
-        System.out.print(employee.getName()); //use a logger instead
-        return "result"; //may want to return a different page name for clarity
+    public String result(@ModelAttribute Employee employee) {
+
+        System.out.print(employee.getName());
+        return "result";
     }
 }
